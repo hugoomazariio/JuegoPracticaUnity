815c3de96a9d0a5f2f7da79c2f6e8263fba98e62
Hugo <160237140+hugoomazariio@users.noreply.github.com>
2024-11-27 01:42:03 +0100

    .

--- PruebaSimple/Assets/Scripts/Enemy/EliminatePlayer.cs
+++ PruebaSimple/Assets/Scripts/Enemy/EliminatePlayer.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class EliminatePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    // Este método se llama cuando el enemigo colisiona con otro objeto, en este caso, se va a hacer que al tocar al player lo elimina.
+    void OnCollisionEnter(Collision collision)
     {
-        
+        // Se verifica si el objeto con el que colisionamos es el jugador
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Destruye al jugador
+            Destroy(collision.gameObject);
+            //Termina la ejecución de unity
+            UnityEditor.EditorApplication.isPlaying=false;
+
+            // Escribe por consola que el jugador ha sido eliminado
+            Debug.Log("El jugador ha sido eliminado. Has perdido el juego.");
+        }
     }
 }
